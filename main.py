ac26bfb64c6bf9a6180a23155146c320da0e6493
Darkhood148 <ujjwal.sharma9999999@gmail.com>
2023-10-18 20:33:29 +0530

    add background

--- main.py
+++ main.py
@@ -8,15 +8,34 @@ win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Brick Breaker")
 FPS = 60
 
+def tileBackground(screen: pygame.display, image: pygame.Surface) -> None:
+    screenWidth, screenHeight = screen.get_size()
+    imageWidth, imageHeight = image.get_size()
+    
+    # Calculate how many tiles we need to draw in x axis and y axis
+    tilesX = math.ceil(screenWidth / imageWidth)
+    tilesY = math.ceil(screenHeight / imageHeight)
+    
+    # Loop over both and blit accordingly
+    for x in range(tilesX):
+        for y in range(tilesY):
+            screen.blit(image, (x * imageWidth, y * imageHeight))
+
 def main():
     clock = pygame.time.Clock()
     run = True
+    
+    bg = pygame.image.load('backg.png')
+    bg = pygame.transform.scale(bg, (400, 300))
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+            
+        tileBackground(win, bg)
+        pygame.display.update()
     pygame.quit()
     quit()
 
